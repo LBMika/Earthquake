1c2889f2bf7b0d66ae4644f2365014d104d84a1e
LMika <lemika.pro@gmail.com>
2021-12-09 10:02:55 +0100

    App entry point

---
+++ src/main/java/fr/mika/worldvulcan/VulcanOfWorldApplication.java
@@ -0,0 +1,13 @@
+package fr.mika.worldvulcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulcanOfWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulcanOfWorldApplication.class, args);
+	}
+
+}

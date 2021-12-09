0a4a428f3284a9bc51f2637b2c34143503020f72
LMika <lemika.pro@gmail.com>
2021-12-09 10:07:32 +0100

    Tests hierarchy

---
+++ src/test/java/fr/mika/worldvulcan/VulcanOfWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mika.worldvulcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulcanOfWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
